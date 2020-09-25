b1079c0ba9d2ab6ba1c7ed04c95e60e8aa611835
tombest1 <caleb_akinola@yahoo.com>
2020-09-25 15:31:41 +0100

    added this

---
+++ even.js
@@ -0,0 +1,167 @@
+// this works
+
+// let num = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let even = []
+
+// function evennum() {
+//     console.log();
+//     for(let i = 0; i < 10; i++){
+
+//         if (num[i]%2 == 0){
+
+//             even.push(num[i]);
+
+//         }
+//     }  
+// }
+// evennum()
+// console.log(even);
+
+
+let email = [ "a@gmail.com", "a@ymail.com", "a@yahoo.com", "b@msn.com", "c@gmail.com"];
+let validmail =[];
+function mail() {
+    
+    for (i = 0; i< email.length; i++){
+
+        if (email[i].includes("@gmail.com")) {
+            validmail.push(email[i]);
+    
+        }
+    }
+    
+    
+}
+mail();
+console.log(validmail);
+
+
+{
+    "firstName": "Oluwatobi",
+    "lastName": "Adeyemi",
+    "location": "shomolu",
+    "phone": 07086431963,	
+    "email": "adeyemitobi12@yahoo.com",	
+    "linkedIn": "https://www.linkedin.com/in/oluwatobi-adeyemi-aca-421633110",
+}
+
+{
+    "firstName": "Caleb ",
+    "lastName": " Akinola",
+    "location": "Shomolu ",
+    "phone": 08189097669 ,	
+    "email": "herotombest@gmail.com",	
+    "linkedIn": "https://www.linkedin.com/in/caleb-akinola-best/"
+}
+{
+    "firstName": "Abiodun",
+    "lastName": "Adedokun",
+    "location": "Shomolu",
+    "phone": 08025741187,	
+    "email": "dokun.abraham@gmail.com",	
+    "linkedIn": "https://www.linkedin.com/in/abiodun-adedokun-a73a581a0/"
+}
+{
+    "firstName": "fauziyat",
+    "lastName": "omoloye",
+    "location": "ogba",
+    "phone": 08100173411,	
+    "email": "omoloyefauziyat@gmail.com",	
+    "linkedIn": "https://www.linkedin.com/in/omoloye-fauziyat-folashade-616876178"
+}
+{
+    "firstName": "Oluwadara",
+    "lastName": "Adenipekun",
+    "location": "Ipaja",
+    "phone": 08131213674 ,	
+    "email": "daraadenipekun97@gmail.com",	
+    "linkedIn": "https://www.linkedin.com/in/oluwadara-adenipekun-13721717"
+}
+{
+    "firstName": "Ladetunji",
+    "lastName": "Osibanjo",
+    "location": "Iju Ishaga",
+    "phone": 	Iju Ishag,	
+    "email": "brandify.ng@gmail.com",	
+    "linkedIn": "https://www.linkedin.com/in/ladetunji-osibanjo-62565827"
+}
+{
+    "firstName": "Utibe",
+    "lastName": "Dennis",
+    "location": "Surulere",
+    "phone": 09025279297,	
+    "email": "utibe.dennis@gmail.com",	
+    "linkedIn": "https://www.linkedin.com/in/utibe-dennis-a4347b1b4/"
+}
+{
+    "firstName": "soteju",
+    "lastName": "Abisola",
+    "location": "ikorodu",
+    "phone": 08186489453 ,	
+    "email": "sotejubisola@gmail.com",	
+    "linkedIn": ""
+}
+{
+    "firstName": "Ayodele",
+    "lastName": "Alademehin",
+    "location": "Ikeja",
+    "phone": 08037797655,	
+    "email": "ayodele.alademehin@gmail.com",	
+    "linkedIn": "https://www.linkedin.com/in/ayodele-alademehin-a8b462121"
+}
+{
+    "firstName": "Irewolede",
+    "lastName": "huzainat",
+    "location": "ikorodu",
+    "phone": 08066498615,	
+    "email": "irewoledehuzainat@gmail.com",	
+    "linkedIn": "https://www.linkedin.com/in/irewolede-uzainah-65a4791b4"
+}
+{
+    "firstName": "ibrahim",
+    "lastName": "adebiyi",
+    "location": "ikorodu",
+    "phone": 08139569356,	
+    "email": "adebiyi01@gmail.com",	
+    "linkedIn": "https://www.linkedin.com/mwlite/in/ibrahim-adebiyi-b7b979166"
+}
+{
+    "firstName": "Ayedun",
+    "lastName": "Daniel",
+    "location": "Ikeja",
+    "phone": 08067843896,	
+    "email": "danielloluwatosin@gmail.com",	
+    "linkedIn": "https://www.linkedin.com/in/ayedun-daniel-oluwatosin-602250b9/"
+
+{
+    "firstName": "Mistura",
+    "lastName": "Quadri",
+    "location": "Okota",
+    "phone": 08154755504,	
+    "email": "quadri.mistura@gmail.com",	
+    "linkedIn": "https://www.linkedin.com/in/mistura-quadri-43b904172"
+}
+{
+    "firstName": "Taiwo",
+    "lastName": "Baptista",
+    "location": "Yaba",
+    "phone": 08181881217,	
+    "email": "davidbaptista25@gmail.com",	
+    "linkedIn": "https://www.linkedin.com/in/taiwo-baptista-89b73b152"
+}
+{
+    "firstName": "James",
+    "lastName": "Ogbaji",
+    "location": "Gbagada",
+    "phone": 07067281222,	
+    "email": "ogbajijames25@gmail.com",	
+    "linkedIn": "https://www.linkedin.com/in/james-ogbaji-9284721b4"
+}
+{
+    "firstName": "Oseafiana ",
+    "lastName": "Mary",
+    "location": "Gbagada",
+    "phone": 09016868148,	
+    "email": "oseafianamary96@gmail.com",	
+    "linkedIn": "https://www.linkedin.com/in/oseafiana-mary-235229137"
+}
